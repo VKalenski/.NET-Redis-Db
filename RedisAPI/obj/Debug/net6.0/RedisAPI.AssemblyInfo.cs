29c031c8b7c0d1fe03a527970ec92ba3de8d02fb
Vilislav Kalenski <vilislavkalenski@gmail.com>
2024-11-01 20:40:05 +0200

    Update from .NET 6.0 to .NET 8.0

--- RedisAPI/obj/Debug/net6.0/RedisAPI.AssemblyInfo.cs
+++ RedisAPI/obj/Debug/net6.0/RedisAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RedisAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c28b481d70e3f76041dbf13a49405f4d453196c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("RedisAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RedisAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
